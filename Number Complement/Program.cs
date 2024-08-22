0889bc21527197ff1bb4722113a45c6ae51dff42
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2024-08-22 18:27:21 +0200

    Add project files.

---
+++ Number Complement/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Number_Complement;
+
+Console.WriteLine("Hello, World!");
+
+
+Solution.getComplement(5);
+
+Solution.getComplement(8);
---
+++ Number Complement/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Complement
+{
+    internal class Solution
+    {
+
+        public static int getComplement(int x)
+        {
+            String final = "";
+            String output = Convert.ToString(x, 2);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (output[i]=='1')
+                {
+                    final += "0";
+                }
+                else
+                {
+                    final += "1";
+                }
+                
+            }
+            int answer = Convert.ToInt32(final, 2);
+            return answer;
+        }
+    }
+}
